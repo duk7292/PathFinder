329d28e86ae0f6117bc1c5e270dbafc3f3e5c3a9
duk7292 <136354450+duk7292@users.noreply.github.com>
2023-07-17 17:34:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("wd")
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("1200x750")
+root.resizable(False, False)
+
+
+text_widget = tk.Text(root)
+text_widget.place(x = 0,y=0,height=750,width=200)
+
+canvas = tk.Canvas(root, width=1000, height=750)
+canvas.place(x=200)
+
+
+
+root.mainloop()
